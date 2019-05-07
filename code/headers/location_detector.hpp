98d2ca32280a51a4720f6a0d133ac8885bae5d68
Lars Versteeg <lars.versteeg1999@gmail.com>
2019-05-07 14:21:31 +0200

    Updated the geographic_coordinate_s.

--- code/headers/location_detector.hpp
+++ code/headers/location_detector.hpp
@@ -1,10 +1,13 @@
 struct geographic_coordinate_s {
-    int latitude;
-    int longtitude;
+    signed int latitude;
+    char NS;
+    signed int longtitude;
+    char EW;
+    int altitude;
 };
 
 class location_detection_c {
 public:
-    virtual void process()= 0;
-    virtual geographic_coordinate_s get_location()= 0;
+    virtual void process() = 0;
+    virtual geographic_coordinate_s get_location() = 0;
 };
