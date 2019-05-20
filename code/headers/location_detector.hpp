d18366663568665daf2a047e06de20fe3146c8e9
Lars <lars.versteeg1999@gmail.com>
2019-05-20 12:52:48 +0200

    Removed methods that didn't belong there and added some documentation.

--- code/headers/location_detector.hpp
+++ code/headers/location_detector.hpp
@@ -5,15 +5,13 @@
 
 namespace r2d2::location_detector {
 
-    class location_detection_c {
+    class module_c : public base_module_c {
     public:
         void process();
 
     protected:
         virtual frame_coordinate_s get_location();
-        GGA parse_nmea(hwlib::string);
-        frame_coordinate_s compress(GGA);
-        frame_coordinate_s compress(uint32_t longitude, uint32_t latitude, bool north, bool east, int16_t altitude);
+        frame_coordinate_s compress(float longitude, float latitude, bool north, bool east, int16_t altitude);
     };
 
 } // namespace r2d2::location_detector
