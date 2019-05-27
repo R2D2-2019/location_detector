a3141774148767052916af1cca8891f7384f27d4
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-05-27 13:19:02 +0200

    Added frame_coordinate_s struct

---
+++ code/headers/frame_coordinate_s.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <hwlib.hpp>
+
+namespace r2d2::location_detector{
+
+    struct frame_coordinate_s{
+        uint8_t latitude_degrees;
+        uint8_t latitude_minutes;
+        uint8_t latitude_seconds;
+        uint16_t latitude_thousandth_second;
+        
+        uint8_t longitude_degrees;
+        uint8_t longitude_minutes;
+        uint8_t longitude_seconds;
+        uint16_t longitude_thousandth_second;
+
+        char north_south_hemisphere;
+        char east_west_hemisphere;
+
+        uint16_t altitude;
+    };
+
+} // namespace r2d2::location_detector
