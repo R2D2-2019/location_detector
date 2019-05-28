ab506560ead10c853a4f694230f62d467365740c
Gianetti <gianluca.piccardo@student.hu.nl>
2019-05-28 13:34:34 +0200

    finished parsing method for strings

--- code/main.cpp
+++ code/main.cpp
@@ -1,29 +1,23 @@
 #include "hwlib.hpp"
 #include <hardware_usart.hpp>
 
-struct location_s{
-  hwlib::string<10> protocol_type;        // 0 type of protocol received
-  uint32_t          time;                 // 1 current time when location was sent
-  uint32_t           latitude;             // 2 east to west
-  char              north_or_south;       // 3 'N' will be north and 'S' will be south
-  uint32_t           longitude;            // 4 north to south
-  char              east_or_west;         // 5 'E' will be east and 'E' will be west
-  uint8_t           fix;                  // 6 Satellite 'fix' w/e that is...
-  uint8_t           number_of_satellites; // 7 Number of satellites tracked
-  uint8_t          horizontal_dilution;  // 8 Not even a clue what this means at the moment
-  uint16_t          altitude;             // 9 height relative to oceanic water surface
-  uint32_t          geoid;                // 10 height of geoid
+struct gga_s{
+  uint32_t  time;                         // 1 current time when location was sent
+  float     latitude;                     // 2 east to west
+  char      north_south_hemisphere;       // 3 'N' will be north and 'S' will be south
+  float     longitude;                    // 4 north to south
+  char      east_west_hemisphere;         // 5 'E' will be east and 'E' will be west
+  uint8_t   fix_quality;                  // 6 Satellite 'fix' w/e that is...
+  uint8_t   satellites_tracked;           // 7 Number of satellites tracked
+  float     horizontal_dilution;          // 8 Not even a clue what this means at the moment
+  float     altitude;                     // 9 height relative to oceanic water surface
+  char      altitude_measurement;
+  float     geoid_height;                 // 10 height of geoid
+  char      geoid_height_measurement;     
 };
-// hwlib::string<10> protocol_namer(const hwlib::string & protocol)const{
-//   hwlib::cout<< protocol << hwlib::endl;
-// }
-////////////////////////////////////////////////////
-//// Function to make the time that is a string ////
-//// an integer for the location struct ////////////
-////////////////////////////////////////////////////
-uint32_t time_maker(const hwlib::string<6> & time){
-  char temp_char;                    //Holds a temporary char from the time string to calculate with
-  uint32_t temp_time = 0;           //Holds a temporary value to place into the new_time
+uint32_t time_maker(const hwlib::string<10> &time){
+  char temp_char;
+  uint32_t temp_time = 0;
   for(unsigned int i = 0; i < time.length(); i++){ 
     temp_char = time[i];
     temp_char -= '0';
@@ -34,11 +28,7 @@ uint32_t time_maker(const hwlib::string<6> & time){
   }
   return temp_time;
 }
-////////////////////////////////////////////////////////////////////////////////
-//////// function to make a latitude or longitude string an integer ////////////
-/////////////////// to place into the frame struct /////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-uint32_t latitude_longitude_maker(const hwlib::string<15> & coordinate){
+float latitude_longitude_maker(const hwlib::string<15> &coordinate){
   char temp_char;
   uint32_t temp_coord = 0;
   for(unsigned int i = 0; i < coordinate.length(); i++){
@@ -51,103 +41,194 @@ uint32_t latitude_longitude_maker(const hwlib::string<15> & coordinate){
       }
     }
   }
-  return temp_coord;
+  return temp_coord / 1000;
 }
-///////////////////////////////////////////////////////////////
-//// function to parse a GPS string to correct sub strings ////
-///////////////////////////////////////////////////////////////
-void parser(const hwlib::string<65> & gps_message){
-  uint8_t              comma_counter = 0;
-  hwlib::string<10>    protocol_temp;
-  hwlib::string<10>    time_temp;
-  hwlib::string<10>    latitude_temp;
-  hwlib::string<10>    north_or_south_temp;
-  hwlib::string<10>    longitude_temp;
-  hwlib::string<10>    east_or_west_temp;
-  hwlib::string<10>    fix_temp;
-  hwlib::string<10>    number_of_satellites_temp;
-  hwlib::string<10>    horizontal_dilution_temp;
-  hwlib::string<10>    altitude_temp;
-  hwlib::string<10>    geoid_temp;
 
+uint8_t fix_maker(const hwlib::string<10> &fix){
+  char temp_char;
+  uint8_t fix_int = 0;
+  for(unsigned int i = 0; i < fix.length(); i++){
+    if(fix[0] == '0'){
+      return 0;
+    }
+    else if(fix[i] > int('0')){
+      temp_char = fix[i];
+      temp_char -= '0';
+      fix_int = temp_char;
+      return fix_int;
+    }
+    else{
+      return 0;
+    }
+  }
+  return fix_int;
+}
 
+uint8_t satellite_maker(const hwlib::string<10> &satellites){
+  char temp_char;
+  uint8_t number_of_satellites = 0;
+  for(unsigned int i = 0; i < satellites.length(); i++){
+    temp_char = satellites[i];
+    temp_char -= '0';
+    if(i == 0){
+      number_of_satellites = temp_char;
+      if(satellites.length() > 1){
+        number_of_satellites *= 10;
+      }
+    }
+    else if(i == 1){
+      number_of_satellites += temp_char;
+    }
+    else{
+      return 0;
+    }
+  }
+  return number_of_satellites;
+}
+
+float horizontal_dilution_maker(const hwlib::string<10> &dilution){
+  char temp_char;
+  float temp_dilution = 0.0;
+  for(unsigned int i = 0; i < dilution.length(); i++){
+    if(dilution != '.') {
+      temp_char = dilution[i];
+      temp_char -= '0';
+      temp_dilution = temp_char;
+      if(i+1 < dilution.length()) {
+        temp_dilution *= 10;
+      }
+    }
+  }
+  return temp_dilution / 10;
+}
+
+float altitude_geoid_maker(const hwlib::string<10> &altitude){
+  char temp_char;
+  float temp_altitude = 0.0;
+  for(unsigned int i = 0; i < altitude.length(); i++){
+    if(altitude[0] == '0'){
+      return 0.0;
+    }
+    if(altitude[i] != '.') {
+      temp_char = altitude[i];
+      temp_char -= '0';
+      temp_altitude += temp_char;
+      if(i+1 < altitude.length()) {
+          temp_altitude *= 10;
+      }
+    }
+  }
+  return temp_altitude / 10 ;
+}
+gga_s parse_nmea(const hwlib::string<65> &gps_message){
+  uint8_t           comma_counter = 0;
+  hwlib::string<10> time ;
+  hwlib::string<10> latitude;
+  char              north_south_hemisphere;
+  hwlib::string<10> longitude;
+  char              east_west_hemisphere;
+  hwlib::string<10> fix_quality;
+  hwlib::string<10> satellites_tracked;
+  hwlib::string<10> horizontal_dilution;
+  hwlib::string<10> altitude;
+  char              altitude_measurement;
+  hwlib::string<10> geoid_height;
+  char              geoid_height_measurement;
   for(unsigned int i = 0; i < gps_message.length(); i++){
     if(gps_message[i] == ','){
       comma_counter++;
       i++;
     }
-    //////////////////////////////////////////////////////////
-    /// cutting up the gps message, due to NMAE GGS format ///
-    /////// I will use the provided comma delimiter //////////
-    ////////////////////////////////////////////////////////// 
     switch (comma_counter){
-      case 0:{
-        protocol_temp.append(gps_message[i]);
+      case 1: {
+        time.append(gps_message[i]);
       }break;
-      case 1:{
-        time_temp.append(gps_message[i]);
+      case 2: {
+        latitude.append(gps_message[i]);
       }break;
-      case 2:{
-        latitude_temp.append(gps_message[i]);
+      case 3: {
+        north_south_hemisphere = gps_message[i];
       }break;
-      case 3:{
-        north_or_south_temp = gps_message[i];
+      case 4: {
+        longitude.append(gps_message[i]);
       }break;
-      case 4:{
-        longitude_temp.append(gps_message[i]);
+      case 5: {
+        east_west_hemisphere = gps_message[i];
       }break;
-      case 5:{
-        east_or_west_temp = gps_message[i];
+      case 6: {
+        fix_quality.append(gps_message[i]);
       }break;
-      case 6:{
-        fix_temp.append(gps_message[i]);
+      case 7: {
+        satellites_tracked.append(gps_message[i]);
       }break;
-      case 7:{
-        number_of_satellites_temp.append(gps_message[i]);
+      case 8: {
+        horizontal_dilution.append(gps_message[i]);
       }break;
-      case 8:{
-        horizontal_dilution_temp.append(gps_message[i]);
-      }
-      case 9:{
-        altitude_temp.append(gps_message[i]);
+      case 9:  {
+        altitude.append(gps_message[i]);
+      }break;
+      case 10:{
+        altitude_measurement = gps_message[i];
       }break;
       case 11:{
-        geoid_temp.append(gps_message[i]);
+        geoid_height.append(gps_message[i]);
       }
+      case 12: {
+        geoid_height_measurement = gps_message[i];
+      }break;
     }
   }
-
   /////////////////////////////////////////////////////////////
   /// some couts for debugging and testing, will be removed ///
   /////////////////////////////////////////////////////////////
-  hwlib::cout << "Protocol used: " << protocol_temp << hwlib::endl;
-  hwlib::cout << "Time is: " << time_temp << hwlib::endl;
-  hwlib::cout << "Latitude is: " << latitude_temp << hwlib::endl;
-  hwlib::cout << "Hemisphere is: " << north_or_south_temp << hwlib::endl;
-  hwlib::cout << "Longitude is: " << longitude_temp << hwlib::endl;
-  hwlib::cout << "Hemisphere is: " << east_or_west_temp << hwlib::endl;
-  hwlib::cout << "Fix is: " << fix_temp << hwlib::endl;
-  hwlib::cout << "Number of satellites: "  << number_of_satellites_temp << hwlib::endl;
-  hwlib::cout << "Horizontal dilution: " << horizontal_dilution_temp <<hwlib::endl;
-  hwlib::cout << "Altitude is: " << altitude_temp << hwlib::endl;
-  hwlib::cout << "Geoid is: " << geoid_temp <<hwlib::endl;
-  hwlib::cout << "amount of commas " << comma_counter << hwlib::endl;
-  hwlib::cout << "==============================================" << hwlib::endl;
-  hwlib::cout << latitude_longitude_maker(latitude_temp) << hwlib::endl;
-  hwlib::cout << latitude_longitude_maker(longitude_temp) << hwlib::endl;
-  hwlib::cout << time_maker(time_temp) << hwlib::endl;
+  hwlib::cout << "Time is: " << '\t'<< '\t' << '\t' << time << hwlib::endl;
+  hwlib::cout << "Latitude is: " << '\t'<< '\t'<< '\t' << latitude << hwlib::endl;
+  hwlib::cout << "Hemisphere is: "  << '\t'<< '\t'<< '\t'<< north_south_hemisphere << hwlib::endl;
+  hwlib::cout << "Longitude is: "<< '\t'<< '\t'<< '\t' << longitude << hwlib::endl;
+  hwlib::cout << "Hemisphere is: "<< '\t'<< '\t'<< '\t' << east_west_hemisphere << hwlib::endl;
+  hwlib::cout << "Fix is: " << '\t'<< '\t'<< '\t'<< fix_quality << hwlib::endl;
+  hwlib::cout << "Number of satellites: " << '\t'<< '\t'<<  satellites_tracked << hwlib::endl;
+  hwlib::cout << "Horizontal dilution: " << '\t'<< '\t'<<  horizontal_dilution <<hwlib::endl;
+  hwlib::cout << "Altitude is: "<< '\t'<< '\t' << '\t'<< altitude << hwlib::endl; 
+  hwlib::cout << "Altitude_measurement is: " << '\t'<< altitude_measurement << hwlib::endl;
+  hwlib::cout << "Geoid is: " << '\t'<< '\t'<< '\t'<< geoid_height <<hwlib::endl;
+  hwlib::cout << "Geoid is: " << '\t'<< '\t'<< '\t'<< geoid_height_measurement <<hwlib::endl;
+  hwlib::cout << "amount of commas "<< '\t'<< '\t' << comma_counter << hwlib::endl;
+
+  gga_s location_package;
+  location_package.time = time_maker(time);
+  location_package.latitude = latitude_longitude_maker(latitude);
+  location_package.north_south_hemisphere = north_south_hemisphere;
+  location_package.longitude = latitude_longitude_maker(longitude);
+  location_package.east_west_hemisphere = east_west_hemisphere;
+  location_package.fix_quality = fix_maker(fix_quality);
+  location_package.satellites_tracked = satellite_maker(satellites_tracked);
+  location_package.horizontal_dilution = horizontal_dilution_maker(horizontal_dilution);
+  location_package.altitude = altitude_geoid_maker(altitude);
+  location_package.altitude_measurement = altitude_measurement;
+  location_package.geoid_height = altitude_geoid_maker(geoid_height);
+  location_package.geoid_height_measurement = geoid_height_measurement;
+
+return location_package;
 }
 
 int main(void) {
   // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
-    hwlib::string <255> gps_message = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"; //GSS Formatted message from module to test function
+    const hwlib::string <100> gps_message = "$GPGGA,123519,4807.038,N,01131.000,E,5,14,0.9,545.4,M,46.9,M,,*47"; //GSS Formatted message from module to test function
+
+    //hwlib::cout << c_string << hwlib::endl;
+
+    
+
     for (unsigned int i = 0; i < 1; i++){
         hwlib::wait_ms(1000);
         hwlib::cout << "this works via arduino" << hwlib::endl;
         hwlib::wait_ms(1000);
       hwlib::cout << gps_message.length() << hwlib::endl;
     }
-    parser(gps_message);
+    gga_s checkit = parse_nmea(gps_message);
+    hwlib::cout << "test cout \n";
+    hwlib::cout << checkit.time << hwlib::endl;
 }
