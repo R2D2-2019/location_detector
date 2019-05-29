8f0b002dd697761436747f9319d4488ed4541c8e
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-05-29 08:35:43 +0200

    Working on the process() function of location_detector
    It isn't finished yet, because we do not know what frame we are listening for.

--- code/src/location_detector.cpp
+++ code/src/location_detector.cpp
@@ -16,8 +16,16 @@ namespace r2d2::location_detector {
 
     void module_c::process() {
         // check for frame
-        // if location request, call get_location
-        // send the location frame on the CAN-bus
+        while (comm.has_data()) {
+            auto frame = comm.get_data();
+
+            if (true) { // TODO: if location request, call get_location
+                frame_coordinate_s location_frame = get_location();
+
+                // send the location frame on the CAN-bus
+                comm.send(location_frame);
+            }
+        }
     }
 
 } // namespace r2d2::location_detector
