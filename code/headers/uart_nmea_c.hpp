1303053ed54e4dfe1fcfa69f8c3008729b75b259
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-05-29 15:45:11 +0200

    Formatted the code according to the style guide and added some doxygen.

--- code/headers/uart_nmea_c.hpp
+++ code/headers/uart_nmea_c.hpp
@@ -25,19 +25,20 @@ namespace r2d2::location_detector {
                            // by only updating the location after a certain
                            // amount of time.
         /// \brief
-        /// 
-        /// \details
-        ///
+        /// Function that creates a frame_coordinate_s struct from a gga_s
+        /// struct.
         frame_coordinate_s compress(const gga_s &source);
         /// \brief
         /// returns time as an integer
         /// \details
-        /// This function return time as an integer by converting it from a hwlib::string
+        /// This function return time as an integer by converting it from a
+        /// hwlib::string
         uint32_t time_maker(const hwlib::string<10> &time);
         /// \brief
         /// returns either a longitude or latitude as a float
         /// \details
-        /// converts a hwlib::string to a calculatable float, useable as a coordinate
+        /// converts a hwlib::string to a calculatable float, useable as a
+        /// coordinate
         float latitude_longitude_maker(const hwlib::string<15> &coordinate);
         /// \brief
         /// returns quality of fix
@@ -60,12 +61,17 @@ namespace r2d2::location_detector {
         /// converts a hwlib::string to a float
         float altitude_geoid_maker(const hwlib::string<10> &altitude);
         /// \brief
-        /// returns a location struct filled with corresponding information/datatypes
-        /// \details
-        /// chops up a hwlib::string and calls other 'maker' functions to fill struct.
+        /// returns a location struct filled with corresponding
+        /// information/datatypes \details chops up a hwlib::string and calls
+        /// other 'maker' functions to fill struct.
         gga_s parse_nmea(const hwlib::string<0> &gps_message);
 
     protected:
+        /// \brief
+        /// Function that returns the current location.
+        /// \details
+        /// get_location first waits for a NMEA GGA sentence on the uart port,
+        /// which it then parses. The result of the parse is returned.
         frame_coordinate_s get_location() override;
 
     public:
--- code/src/location_detector.cpp
+++ code/src/location_detector.cpp
@@ -2,21 +2,39 @@
 
 namespace r2d2::location_detector {
 
-    frame_coordinate_s module_c::compress(float longitude, float latitude, bool north, bool east, int16_t altitude) {
+    frame_coordinate_s module_c::compress(float longitude, float latitude,
+                                          bool north, bool east,
+                                          int16_t altitude) {
 
         uint8_t longitude_degrees = longitude;
         uint8_t longitude_minutes = (longitude - longitude_degrees) * 60;
-        uint8_t longitude_seconds = ((longitude - longitude_degrees) * 60 - longitude_minutes) * 60;
-        uint8_t longitude_thousandths_second = (((( longitude - longitude_degrees) * 60 - longitude_minutes) * 60) - longitude_seconds) * 1000;
+        uint8_t longitude_seconds =
+            ((longitude - longitude_degrees) * 60 - longitude_minutes) * 60;
+        uint8_t longitude_thousandths_second =
+            ((((longitude - longitude_degrees) * 60 - longitude_minutes) * 60) -
+             longitude_seconds) *
+            1000;
 
         uint8_t latitude_degrees = latitude;
         uint8_t latitude_minutes = (latitude - latitude_degrees) * 60;
-        uint8_t latitude_seconds = ((latitude - latitude_degrees) * 60 - latitude_minutes) * 60;
-        uint8_t latitude_thousandths_second = (((( latitude - latitude_degrees) * 60 - latitude_minutes) * 60) - latitude_seconds) * 1000;
+        uint8_t latitude_seconds =
+            ((latitude - latitude_degrees) * 60 - latitude_minutes) * 60;
+        uint8_t latitude_thousandths_second =
+            ((((latitude - latitude_degrees) * 60 - latitude_minutes) * 60) -
+             latitude_seconds) *
+            1000;
 
-        return frame_coordinate_s {latitude_degrees, latitude_minutes, latitude_seconds, latitude_thousandths_second, longitude_degrees, longitude_minutes, longitude_seconds,
-                                    longitude_thousandths_second, north, east, altitude};
-
+        return frame_coordinate_s{latitude_degrees,
+                                  latitude_minutes,
+                                  latitude_seconds,
+                                  latitude_thousandths_second,
+                                  longitude_degrees,
+                                  longitude_minutes,
+                                  longitude_seconds,
+                                  longitude_thousandths_second,
+                                  north,
+                                  east,
+                                  altitude};
     }
 
     void module_c::process() {
