29be81978d1a62c41d4b070f00485e053c3272f6
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-06-04 15:26:20 +0200

    changed the comments

--- code/headers/uart_nmea_c.hpp
+++ code/headers/uart_nmea_c.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <gga.hpp>
 #include <frame_types.hpp>
-#include <usart_connection.hpp>
-#include <nmea_parser.hpp>
+#include <gga.hpp>
 #include <nmea_listener.hpp>
+#include <nmea_parser.hpp>
+#include <usart_connection.hpp>
 
 namespace r2d2::location {
     /// \brief
@@ -12,23 +12,19 @@ namespace r2d2::location {
     /// sentences on an uart/usart serial port.
     /// \details
     /// This class uses an uart/usart port to inteface with a GNSS
-    /// module to determine its location. It inherits from module_c which means
-    /// it has a progress() function. In this function the class will check if
-    /// it received any messages on the CAN-bus. If it received a location
-    /// request it will send a frame containing information about the location
-    /// on the CAN-bus. This class will only work if the GNSS module outputs
-    /// NMEA gga messages.
+    /// module to determine its location.  This class will only work if the GNSS
+    /// module outputs NMEA gga messages.
     class uart_nmea_c {
     private:
         /**
          * @brief Nmea parser for parsing all the strings
-         * 
+         *
          */
         nmea_parser_c parser;
 
         /**
          * @brief usart listener for getting a string of nmea data.
-         * 
+         *
          */
         nmea_listener listener;
 
@@ -42,8 +38,8 @@ namespace r2d2::location {
         /// \brief
         /// Function that returns the current location.
         /// \details
-        /// get_location first waits for a NMEA GGA sentence on the uart port,
-        /// which it then parses. The result of the parse is returned.
+        /// when no new location is available this will return a gga_s struct
+        /// with a fix_quality on 0 for an invalid fix.
         gga_s get_location();
 
         /// \brief
@@ -52,7 +48,7 @@ namespace r2d2::location {
         frame_coordinate_s gga_to_frame(const gga_s &source);
 
         /**
-         * \brief This method is used to manually make coordinate frames.
+         * \brief This method is used to create a coordinate frames.
          *
          * \param longitude
          * \param latitude
@@ -60,9 +56,9 @@ namespace r2d2::location {
          * \param south
          * \param altitude
          */
-        frame_coordinate_s coordinates_to_frame(float longitude, float latitude, bool north,
-                                    bool east, int16_t altitude);
-
+        frame_coordinate_s coordinates_to_frame(float longitude, float latitude,
+                                                bool north, bool east,
+                                                int16_t altitude);
     };
 
 } // namespace r2d2::location
