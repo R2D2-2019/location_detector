d6339399c4a7bb11f4f4b3d39a139ca854cb086a
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-06-23 16:25:22 +0200

    added documentation to the gga message

--- code/headers/gga.hpp
+++ code/headers/gga.hpp
@@ -11,18 +11,48 @@ namespace r2d2::location {
      *
      */
     struct gga_s {
+        // UTC time of fix
         uint32_t time;
-        float latitude;
-        float longitude;
+
+        // Latitude/ longitude
+        uint32_t latitude;
+        uint32_t longitude;
+
+        // Horizontal dilution of precision. The lower the number the better.
+        // This number is lower if the satellites are more spread out.
+        // https://the5krunner.com/2018/07/18/gps-accuracy-hdop-pdop-gdop-multipath-the-atmosphere/
         float horizontal_dilution;
+
+        // Altitude above sea level. The precision of the altitude is always
+        // lower than the lat/long. The precision of the altitude is not in the
+        // gga message.
         float altitude;
-        float geoid_height;        
+
+        // altitude using the WGS-84 ellipsoid
+        float geoid_height;
+
+        // variable for how reliable the data is.
+        // 0 = invalid
+        // 1 = GPS fix (SPS)
+        // 2 = DGPS fix, OmniSTAR VBS
+        // 3 = PPS fix
+        // 4 = Real Time Kinematic, fixed integers
+        // 5 = Float RTK, float integers, OmniSTAR XP/HP or Location RTK
+        // 6 = estimated (dead reckoning) (2.3 feature)
+        // 7 = Manual input mode
+        // 8 = Simulation mode
         uint8_t fix_quality;
+
+        // variable that stores how much gps satellites we are tracking.
         uint8_t satellites_tracked;
-        char north_south_hemisphere;
-        char east_west_hemisphere;
-        char altitude_measurement;
-        char geoid_height_measurement;
+
+        // bool for what side of the hemispere the location is
+        bool is_north_hemisphere;
+        bool is_east_hemisphere;
+
+        // units for altitude and geoid separation
+        char altitude_unit;
+        char geoid_height_unit;
     };
 
 } // namespace r2d2::location
